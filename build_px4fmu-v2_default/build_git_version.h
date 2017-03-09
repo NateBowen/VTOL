6389548d8cef1b4eff325af7d4064922132fd649
Nate Bowen <nate_bowen@rocketmail.com>
2017-03-09 17:20:18 -0500

    Build firmware with dummy RVJET configuration

--- build_px4fmu-v2_default/build_git_version.h
+++ build_px4fmu-v2_default/build_git_version.h
@@ -2,6 +2,6 @@
 /* Auto Magically Generated file */
 /* Do not edit! */
 #pragma once
-#define PX4_GIT_VERSION_STR  "0c926106f1ea3be02033774d365520c4e2dc4d43"
-#define PX4_GIT_VERSION_BINARY 0x0c926106f1ea3be0
-#define PX4_GIT_TAG_STR  "v1.5.0-2230-g0c92610"
+#define PX4_GIT_VERSION_STR  "08ef342632c8f061bb85778ec61d9b0d4b2193ac"
+#define PX4_GIT_VERSION_BINARY 0x08ef342632c8f061
+#define PX4_GIT_TAG_STR  "v1.5.0-2232-g08ef342"
