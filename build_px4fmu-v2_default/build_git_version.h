36a8eb3b7c94482f59360a447cc659707f50c290
Nate Bowen <nate_bowen@rocketmail.com>
2017-03-14 15:00:52 -0400

    Build firmware.

--- build_px4fmu-v2_default/build_git_version.h
+++ build_px4fmu-v2_default/build_git_version.h
@@ -2,6 +2,6 @@
 /* Auto Magically Generated file */
 /* Do not edit! */
 #pragma once
-#define PX4_GIT_VERSION_STR  "3b62fec22235348b7badec3a5a19ab45ff0271a8"
-#define PX4_GIT_VERSION_BINARY 0x3b62fec22235348b
-#define PX4_GIT_TAG_STR  "v1.5.0-2253-g3b62fec"
+#define PX4_GIT_VERSION_STR  "cfbbbce8155ae252ac2c51429cf5eda9b6893743"
+#define PX4_GIT_VERSION_BINARY 0xcfbbbce8155ae252
+#define PX4_GIT_TAG_STR  "v1.5.0-2255-gcfbbbce"
