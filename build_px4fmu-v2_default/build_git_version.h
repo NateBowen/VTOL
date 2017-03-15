8ecf076fb3193378a04750c1c8187ae5c67ed2cc
Nate Bowen <nate_bowen@rocketmail.com>
2017-03-15 13:57:33 -0400

    Change to Control Group 0. Build firmware.

--- build_px4fmu-v2_default/build_git_version.h
+++ build_px4fmu-v2_default/build_git_version.h
@@ -2,6 +2,6 @@
 /* Auto Magically Generated file */
 /* Do not edit! */
 #pragma once
-#define PX4_GIT_VERSION_STR  "7e023538d5331ba8ad83b89e369ca7e65e950f2d"
-#define PX4_GIT_VERSION_BINARY 0x7e023538d5331ba8
-#define PX4_GIT_TAG_STR  "v1.5.0-2346-g7e02353"
+#define PX4_GIT_VERSION_STR  "c3a552c41b98cf455eaf9dcb23d64c74dd0337dc"
+#define PX4_GIT_VERSION_BINARY 0xc3a552c41b98cf45
+#define PX4_GIT_TAG_STR  "v1.5.0-2347-gc3a552c"
