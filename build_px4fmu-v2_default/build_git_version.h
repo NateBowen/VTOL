8bd32f777ef43c7ff571963393b7fc2dc375f6c0
Nate Bowen <nate_bowen@rocketmail.com>
2017-03-24 18:17:24 -0400

    Build firmware. Nate's PC Toolchain.

--- build_px4fmu-v2_default/build_git_version.h
+++ build_px4fmu-v2_default/build_git_version.h
@@ -2,6 +2,6 @@
 /* Auto Magically Generated file */
 /* Do not edit! */
 #pragma once
-#define PX4_GIT_VERSION_STR  "e95e7d09e9763c53217bc15c2452aec4e6a165a4"
-#define PX4_GIT_VERSION_BINARY 0xe95e7d09e9763c53
-#define PX4_GIT_TAG_STR  "v1.5.0-2353-ge95e7d0"
+#define PX4_GIT_VERSION_STR  "4062a3dc0d86f0c065249f131764e1049e57ecdb"
+#define PX4_GIT_VERSION_BINARY 0x4062a3dc0d86f0c0
+#define PX4_GIT_TAG_STR  "v1.5.0-2355-g4062a3d"
